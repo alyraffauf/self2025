d4db4949e7be37daacaace3634f1622fe9ea79bd
Aly Raffauf <aly@aly.codes>
2025-06-11 22:36:52 -0400

    flake: update description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My aly.codes website.";
+  description = "Stop Breaking Things: A Gentle Introduction to NixOS in the Homelab";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -24,6 +24,29 @@
           };
         });
   in {
+    packages = forAllSystems ({pkgs}: {
+      default = pkgs.stdenv.mkDerivation {
+        buildPhase = ''
+          marp index.md
+        '';
+
+        installPhase = ''
+          mkdir $out
+          cp index.html $out/
+          cp -r img $out/
+        '';
+
+        name = "stop-breaking-things";
+        nativeBuildInputs = [pkgs.marp-cli];
+        src = self;
+
+        version =
+          if self ? shortRev
+          then "git-${self.shortRev}"
+          else "dev";
+      };
+    });
+
     devShells = forAllSystems ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs; [
